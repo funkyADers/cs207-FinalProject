414653353a6032816aebe39fabecff1bc9e854a7
Fabio Pruneri <fabiopruneri@college.harvard.edu>
2019-11-07 13:36:22 -0500

    Completed operator overloading and basic trig functions

--- funkyAD/base.py
+++ funkyAD/base.py
@@ -9,16 +9,19 @@ class AD():
         self.n = None
         self.m = None
 
+    def grad(self, *args):
+        out_nodes = self._evaluate(*args)
+        return np.array([node.d for node in unpack(out_nodes)])
+
     def set_seed(self, seed):
         self.seed = seed
 
-
     def _check_seed(self, l):
-        # Checks if seed has appropriate dimension
         if len(seed) != l:
             raise ValueError("Seed dimension does not match input dimension")
 
     def _evaluate(self, *args):
+
         # Compute number of inputs
         self.n = count_recursive(args)
 
@@ -45,38 +48,101 @@ class AD():
         return self.f(*new_args)
 
 
+
+
 class Node():
 
     def __init__(self, v, d=0):
         self.v = v
         self.d = d # If derivative is None it's assumed to be 0 (for a constant node)
-        
+
         #self.prev = []
         #self.next = []
 
+    def __add__(self, other):
+        return addition(self, other)
+    def __radd__(self, other):
+        return addition(self, other)
+    def __sub__(self, other):
+        return addition(self, -other)
+    def __rsub__(self, other):
+        return addition(-self, other)
+    def __mul__(self, other):
+        return multiplication(self, other)
+    def __rmul__(self, other):
+        return multiplication(self, other)
+    def __pow__(self, other):
+        return power(self, other)
+    def __rpow__(self, other):
+        ## TODO
+        raise NotImplemented
+    def __floordiv__(self, other):
+        return floordiv(self, other)
+    def __rfloordiv__(self, other):
+        return floordiv(other, self)
+    def __div__(self, other):
+        return division(self, other)
+    def __rdiv__(self, other):
+        return division(other, self)
+
+    # TODO
+    #def __iadd__(self, other): etc
+
+    def __pos__(self):
+        return pos(self)
+    def __neg__(self):
+        return neg(self)
+    def __abs__(self):
+        return _abs(self)
+    def __invert__(self):
+        return invert(self)
+    def __round__(self, n):
+        return _round(self, n)
+    def __floor__(self):
+        return floor(self)
+    def __ceil__(self):
+        return ceil(self)
+    def __trunc__(self):
+        return trunc(self)
+
+
+    def __eq__(self, other):
+        return self.v.__eq__(other.v)
+    def __ne__(self, other):
+        return self.v.__ne__(other.v)
+    def __lt__(self, other):
+        return self.v.__lt__(other.v)
+    def __gt__(self, other):
+        return self.v.__gt__(other.v)
+    def __le__(self, other):
+        return self.v.__le__(other.v)
+    def __ge__(self, other):
+        return self.v.__ge__(other.v)
+
+    def __int__(self):
+        return self.__floor__(self)
+    def __long__(self):
+        return self.__floor__(self)   
+    def __float__(self):
+        return self
+    def __complex__(self):
+        raise NotImplemented("Complex number not supported")
+        
     def __str__(self):
         return "Node object with value " + str(self.v) + " and derivative " + str(self.d)
 
-    def __add__(self, other):
-        return addition(self, other)
+    def __repr__(self):
+        return "Node(" + str(self.v) + ", " + str(self.d) + ")"
 
-    def __radd__(self, other):
-        return addition(self, other)
-
-    def __mul__(self, other):
-        return multiplication(self, other)
-
-    def __rmul__(self, other):
-        return multiplication(self, other)
-
-    def __pow__(self, other):
-        return power(self, other)
 
 def grad(f):
-    # Syntactic sugar for AD(f).grad
     return AD(f).grad
 
 # Import statement has to be at the bottom for some reason
-from functions import addition, multiplication, power
-from helpers import count_recursive, nodify
+from functions import addition, multiplication, division, power, pos, neg, _abs, invert, \
+                        floordiv, _round, floor, ceil, trunc
+from helpers import count_recursive, nodify, unpack
 
+if __name__ == "__main__":
+    print(Node(4, 5))
+    print(AD(lambda x: x ** 2))
