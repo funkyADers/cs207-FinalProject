54ccd37045b27423c30207ce52f80b042a9092ba
Johannes Kolberg <johannes.kolberg@cognite.com>
2019-11-15 02:49:49 -0500

    Update travis and setup to test installed package

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/funkyADers/cs207-FinalProject",
-    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src', exclude=('tests',))
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
