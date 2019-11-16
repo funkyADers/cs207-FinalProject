bad5340de77689cd785da8123d6797c6b80799dd
Johannes Kolberg <johannes.kolberg@cognite.com>
2019-11-16 16:42:02 -0500

    Consolidate tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,63 +3,56 @@ from funkyAD.base import Node
 from funkyAD.functions import addition, multiplication, division, floordiv, power
 
 # No need to test for invalid Nodes, those are handled in the creation of Nodes
+# Only need one overload test each; if that works, all other variations via overloads will too
 
-# Addition valid Nodes
-def test_node_add():
+def test_addition():
+    # Two nodes via function call
     assert addition(Node(1, 2), Node(3, 4)) == Node(4, 6)
-
-# Addition overload
-def test_node_add_overload():
+    # Two nodes via overload
     assert Node(1, 2) + Node(3, 4) == Node(4, 6)
-
-# Addition constants
-def test_add_constants():
+    # Two constants via function call
     assert addition(1, 2) == Node(3, 0)
+    # Node and constant
+    assert addition(Node(1, 2), 2) == Node(3, 2)
 
-# Multiplication valid Nodes
-def test_node_multiply():
+def test_multiplication():
+    # Two nodes via function call
     assert multiplication(Node(1, 2), Node(3, 4)) == Node(3, 10)
-
-# Multiplication overload
-def test_node_multiply_overload():
+    # Two nodes via overload
     assert Node(1, 2) * Node(3, 4) == Node(3, 10)
-
-# Multiplication constants
-def test_multiply_constants():
+    # Two constants via function call
     assert multiplication(1, 2) == Node(2, 0)
-
-# Division valid Nodes
-def test_node_division():
+    # Node and constant
+    assert multiplication(Node(2, 2), 2) == Node(4, 4)
+    
+def test_division():
+    # Two nodes via function call
     assert division(Node(5, 3), Node(2, 4)) == Node(2.5, -3.5)
-
-# Division overload
-def test_node_division_overload():
+    # Two nodes via overload
     assert Node(5, 3) / Node(2, 4) == Node(2.5, -3.5)
-
-# Division constants
-def test_division_constants():
+    # Two constants via function call
     assert division(4, 2) == Node(2, 0)
+    # Node and constant
+    assert division(Node(4, 4), 2) == Node(4, 4) / 2 == Node(2, 2)
+    assert 6 / Node(3, 3) == Node(2, -2)
 
-# Floor division valid Nodes
-def test_node_floordivision():
+def test_floordivision():
+    # Two nodes via function call
     assert floordiv(Node(8, 8), Node(3, 3)) == Node(2, 0)
-
-# Floor division overload
-def test_node_floordivision_overload():
+    # Two nodes via overload
     assert Node(8, 8) // Node(3, 3) == Node(2, 0)
-
-# Floor division constants
-def test_floordivision_constants():
+    # Two constants via function call
     assert floordiv(11, 3) == Node(3, 0)
+    # Node and constant
+    assert floordiv(Node(7.5, 3.2), 2) == Node(7.5, 3.2) // 2 == Node(3, 0)
 
-# Power valid Nodes
-def test_nodes_power():
+def test_power():
+    # Two nodes via function call
     assert power(Node(2, 2), Node(3, 3)) == Node(8, 24)
-
-# Power overload
-def test_nodes_power_overload():
+    # Two nodes via overload
     assert Node(2, 2) ** Node(3, 3) == Node(8, 24)
-
-# Power constants
-def test_constants_power():
+    # Two constants via function call
     assert power(4, 2) == Node(16, 0)
+    # Node and constant
+    assert Node(3, 3) ** 2 == Node(9, 18)
+
