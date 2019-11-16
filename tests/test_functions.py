82ab10811eaa4f7dd9118db0939cfcf15239cc57
Johannes Kolberg <johannes.kolberg@cognite.com>
2019-11-16 15:18:38 -0500

    Change to specific imports rather than all

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,27 @@
 import pytest
 from funkyAD.base import Node
-from funkyAD.functions import *
+from funkyAD.functions import add, addition, mul, multiplication
 
+# Addition valid Nodes
 def test_node_add():
-    assert add(Node(1, 2), Node(3, 4)) == Node(4, 6)
+    assert addition(Node(1, 2), Node(3, 4)) == Node(4, 6)
 
-def test_node_addition_overload():
+# Addition overload
+def test_node_add_overload():
     assert Node(1, 2) + Node(3, 4) == Node(4, 6)
 
+# Addition constants
 def test_add_constants():
-    assert add(1, 2) == Node(3, 0)
+    assert addition(1, 2) == Node(3, 0)
+
+# Multiplication valid Nodes
+def test_node_multiply():
+    assert mul(Node(1, 2), Node(3, 4)) == Node(3, 10)
+
+# Multiplication overload
+def test_node_multiply_overload():
+    assert Node(1, 2) * Node(3, 4) == Node(3, 10)
+
+# Multiplication constants
+def test_multiply_constants():
+    assert mul(1, 2) == Node(2, 0)
