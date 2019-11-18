fec2e2fb5c1def3f939887f9e9e4503865d184f1
Tyler Yoo <tyler_yoo@g.harvard.edu>
2019-11-18 18:58:28 -0500

    added tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -12,6 +12,14 @@ def test_count_recursive_list():
     x = [1,2,3]
     assert count_recursive(x)==3
 
+def test_count_recursive_ndarray():
+    x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    assert count_recursive(x)==6
+
+def test_count_recursive_invalid_input():
+    with pytest.raises(TypeError):
+        count_recursive(6)
+        
 def test_unpack_ndarray():
     x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
     assert unpack(x)==[1,2,3,4,5,6]
@@ -19,6 +27,10 @@ def test_unpack_ndarray():
 def test_unpack_ndlist():
     x = [[1,2,],[3,4]]
     assert unpack(x)==[1,2,3,4]
+
+def test_unpack_invalid_input():
+    with pytest.raises(TypeError):
+        unpack("text")
 
 def test_nodify_nparray():
     x = np.array([1,2,3])
@@ -30,4 +42,6 @@ def test_nodify_list():
     seed = [1,2,3]
     assert nodify(x, seed)==[Node(1,1), Node(2,2), Node(3,3)]
 
-
+def test_nodify_invalid_input():
+    with pytest.raises(TypeError):
+        nodify(3.14)
