ce79c2c2bf4c2cc1f161365cc802a40a3ec727ff
Tyler Yoo <tyler_yoo@g.harvard.edu>
2019-11-18 23:16:16 -0500

    solved Node import issues in unpack/count

--- src/funkyAD/helpers.py
+++ src/funkyAD/helpers.py
@@ -3,10 +3,11 @@ import numpy as np
 
 def count_recursive(args):
     '''Counts the number of arguments by recursing over np.arrays and lists'''
+    from .base import Node
     if isinstance(args, np.ndarray) or isinstance(args, list) or isinstance(args, tuple):
     
         pass
-    elif isinstance(args, int) or isinstance(args, float) or isinstance(args, Node()):
+    elif isinstance(args, int) or isinstance(args, float) or isinstance(args, Node):
         return 1
     else:
         raise TypeError('The input argument should be either np.arrays or list')
@@ -26,7 +27,8 @@ def count_recursive_recursion_part(args):
 
 def unpack(args):
     '''Unpacks items in nested np.arrays or lists into a depth-1 list'''
-    if isinstance(args, np.ndarray) or isinstance(args, list) or isinstance(args, Node()):
+    from .base import Node
+    if isinstance(args, np.ndarray) or isinstance(args, list) or isinstance(args, Node):
         pass
     else:
         raise TypeError('The input argument should be either np.arrays or list')
