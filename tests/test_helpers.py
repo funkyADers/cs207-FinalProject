b763cbf614dd625d3c9bf2d15772b9096ba3d076
Tyler Yoo <tyler_yoo@g.harvard.edu>
2019-11-18 22:04:30 -0500

    minor changes in test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -20,9 +20,13 @@ def test_count_recursive_invalid_input():
     with pytest.raises(TypeError):
         count_recursive(6)
         
-def test_unpack_ndarray():
+def test_unpack_2darray():
     x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
     assert unpack(x)==[1,2,3,4,5,6]
+
+def test_unpack_3darray():
+    y = np.array([[[1,2],[3,4]],[[5,6],[7,8]],[[9,10],[11,12]]])
+    assert unpack(y) = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 def test_unpack_ndlist():
     x = [[1,2,],[3,4]]
