47864d8de1725feda84d9ff74c84bab8dce65601
tyler-yoo94 <54858279+tyler-yoo94@users.noreply.github.com>
2019-12-10 00:03:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="funkyAD-funkyADers", 
-    version="0.0.6",
+    version="1.0.0",
     author="Anna Zink, Johannes K. Kolberg, Fabio Pruneri, Tyler Yoo",
     author_email="",
     description="An implementation of Auto Differentiaion",
